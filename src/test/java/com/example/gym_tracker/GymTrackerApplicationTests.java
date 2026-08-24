1167d16bdb0965ff0942f9a4a2dc1ca2b55bb0bb
shiven-puri <shiven.puri@proton.me>
2026-08-24 20:35:35 +0530

    Add H2 test profile for CI

--- src/test/java/com/example/gym_tracker/GymTrackerApplicationTests.java
+++ src/test/java/com/example/gym_tracker/GymTrackerApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.gym_tracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GymTrackerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
